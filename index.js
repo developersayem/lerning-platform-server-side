15406da458fec4e704de2f16f6cdf61496d728f0
munna vail <98878071+devsayemmolla@users.noreply.github.com>
2022-10-25 17:35:52 +0600

    courses congit added

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const courses = require("./data/courses.json");
+const coursesCategories = require("./data/courses-categories.json");
+
+app.get("/", (req, res) => {
+  res.send("Welcom!");
+});
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
