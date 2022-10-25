89d02a89709c1336a81d5f47a1531df85a134eaf
munna vail <98878071+devsayemmolla@users.noreply.github.com>
2022-10-26 01:55:55 +0600

    cors bug fixed

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+app.use(cors({ origin: `*` }));
 
 const courses = require("./data/courses.json");
 const coursesCategories = require("./data/courses-categories.json");
